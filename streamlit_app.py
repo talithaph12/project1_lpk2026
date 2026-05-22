c67ba6425557f039462e1efd28bb5b9aa289cb53
talithaph12 <talithaph124@gmail.com>
2026-05-22 14:22:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,7 @@ a soft return is used for the next line.
 Two (or more) newline characters in a row will result in a hard return.
 '''
 st.markdown(multi)
+
+import streamlit as st
+
+st.balloons()
