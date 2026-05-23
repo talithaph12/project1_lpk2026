fae75af3ca70f268488eca2eba2399c69c24ef77
talithaph12 <talithaph124@gmail.com>
2026-05-23 10:11:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-st.title("📊 Dashboard")
-st.header("Laporan Bulanan")
-st.subheader("📈 Monthly Expenses")
-st.caption("Made with ❤️ using Streamlit")
-
 st.set_page_config(
   page_title="Finance Dashboard",
   layout="wide"
 )
+
+st.title("📊 Dashboard")
+st.header("Laporan Bulanan")
+st.subheader("📈 Monthly Expenses")
+st.caption("Made with ❤️ using Streamlit")
